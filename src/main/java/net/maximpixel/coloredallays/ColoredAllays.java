9ed38862c529218ad1180515ed5e6b0c18500181
Maxim <maximpixel@yandex.ru>
2022-04-23 12:26:23 +0300

    Fabric init

---
+++ src/main/java/net/maximpixel/coloredallays/ColoredAllays.java
@@ -0,0 +1,10 @@
+package net.maximpixel.coloredallays;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ColoredAllays implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
